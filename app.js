7e792b8640cc88b52a10deaabcb40a85d3c749ed
Spawn9986 <scouch9986@gmail.com>
2023-06-23 10:12:21 -0500

    fixed the buttons which were previously unclickable

---
+++ app.js
@@ -0,0 +1,11 @@
+/* let myBtn = document.querySelectorAll("button");
+let output = document.querySelector("output");
+const calculator = document.querySelector(".calculator");
+
+// Add an event listener to the parent element
+calculator.addEventListener("click", (event) => {
+  if (event.target.matches("myBtn")) {
+    output.innerText = 12345678;
+  }
+}); */
+​
