a3d6dc4a3ae4be500a73c3fc9d1f8c8440db11df
Spawn9986 <scouch9986@gmail.com>
2023-06-23 12:13:03 -0500

    added some event listeners

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-/* let myBtn = document.querySelectorAll("button");
-let output = document.querySelector("output");
+let output = document.querySelector(".output");
 const calculator = document.querySelector(".calculator");
+let placeHolder = "0";
 
-// Add an event listener to the parent element
-calculator.addEventListener("click", (event) => {
-  if (event.target.matches("myBtn")) {
-    output.innerText = 12345678;
+function buttonClick(value) {
+  //when a button is clicked change the output inner text to match that of the clicked button if its a number
+  //output.innerText = value;
+  if (isNaN(parseInt(value))) {
+    output.innerText = placeHolder;
+    /// DO something handlesymbol function etc
+  } else {
+    output.innerText = value;
+    // Do something handleNumber function
   }
-}); */
-​
+}
+
+function init() {
+  // Add an event listener to the parent element
+  calculator.addEventListener("click", function (event) {
+    buttonClick(event.target.innerText);
+  });
+}
+
+init();
